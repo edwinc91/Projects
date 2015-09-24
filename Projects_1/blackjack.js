4c8a97ac441895cb13e2934f686217b3459eaff2
Edwin Choi <edwinc91@Edwin-Chois-MacBook-Pro.local>
2015-09-24 11:29:55 -0400

    added the deck and some basic objects into the js of the game

---
+++ Projects_1/blackjack.js
@@ -0,0 +1,73 @@
+var deck = [
+  {Card: '2', Suit: 'Diamond'},
+  {Card: '2', Suit: 'Clover'},
+  {Card: '2', Suit: 'Heart'},
+  {Card: '2', Suit: 'Spade'},
+  {Card: '3', Suit: 'Diamond'},
+  {Card: '3', Suit: 'Clover'},
+  {Card: '3', Suit: 'Heart'},
+  {Card: '3', Suit: 'Spade'},
+  {Card: '4', Suit: 'Diamond'},
+  {Card: '4', Suit: 'Clover'},
+  {Card: '4', Suit: 'Heart'},
+  {Card: '4', Suit: 'Spade'},
+  {Card: '5', Suit: 'Diamond'},
+  {Card: '5', Suit: 'Clover'},
+  {Card: '5', Suit: 'Heart'},
+  {Card: '5', Suit: 'Spade'},
+  {Card: '6', Suit: 'Diamond'},
+  {Card: '6', Suit: 'Clover'},
+  {Card: '6', Suit: 'Heart'},
+  {Card: '6', Suit: 'Spade'},
+  {Card: '7', Suit: 'Diamond'},
+  {Card: '7', Suit: 'Clover'},
+  {Card: '7', Suit: 'Heart'},
+  {Card: '7', Suit: 'Spade'},
+  {Card: '8', Suit: 'Diamond'},
+  {Card: '8', Suit: 'Clover'},
+  {Card: '8', Suit: 'Heart'},
+  {Card: '8', Suit: 'Spade'},
+  {Card: '9', Suit: 'Diamond'},
+  {Card: '9', Suit: 'Clover'},
+  {Card: '9', Suit: 'Heart'},
+  {Card: '9', Suit: 'Spade'},
+  {Card: '10', Suit: 'Diamond'},
+  {Card: '10', Suit: 'Clover'},
+  {Card: '10', Suit: 'Heart'},
+  {Card: '10', Suit: 'Spade'},
+  {Card: 'Jack', Suit: 'Diamond'},
+  {Card: 'Jack', Suit: 'Clover'},
+  {Card: 'Jack', Suit: 'Heart'},
+  {Card: 'Jack', Suit: 'Spade'},
+  {Card: 'Queen', Suit: 'Diamond'},
+  {Card: 'Queen', Suit: 'Clover'},
+  {Card: 'Queen', Suit: 'Heart'},
+  {Card: 'Queen', Suit: 'Spade'},
+  {Card: 'King', Suit: 'Diamond'},
+  {Card: 'King', Suit: 'Clover'},
+  {Card: 'King', Suit: 'Heart'},
+  {Card: 'King', Suit: 'Spade'},
+  {Card: 'Ace', Suit: 'Diamond'},
+  {Card: 'Ace', Suit: 'Clover'},
+  {Card: 'Ace', Suit: 'Heart'},
+  {Card: 'Ace', Suit: 'Spade'}
+];
+
+var blackjack = {
+  cards: deck,
+  DealtCards: [],
+  rounds: 0,
+  inPlay: {
+    PlayerCards: [],
+    DealerCards: [],
+  },
+  usedCards: [],
+  PlayerCardValue: undefined,
+  DealerCardValue: undefined,
+  dealPlayerCard1: function () {
+    var PlayerFirstCardDealtRandomizedNumber = Math.floor(Math.random() * decks.length);
+    var PlayerActualFirstCard = decks[PlayerFirstCardDealtRandomizedNumber];
+    deck.splice(PlayerFirstCardDealtRandomizedNumber, 1);
+    blackjack.inPlay.playerCards.push(PlayerActualFirstCard);
+  }
+}
